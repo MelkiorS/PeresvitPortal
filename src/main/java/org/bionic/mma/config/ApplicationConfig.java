46bb2c8ebd2714795581ffde4c4bcf09c2571c5e
Vitaliy Kornienko <vkornienko85@gmail.com>
2016-10-23 12:18:29 +0300

    Test ApplicationConfig

--- src/main/java/org/bionic/mma/config/ApplicationConfig.java
+++ src/main/java/org/bionic/mma/config/ApplicationConfig.java
@@ -1,7 +1,23 @@
 package org.bionic.mma.config;
 
-/**
- * Created by maximmaximov_2 on 22.10.16.
- */
-public class ApplicationConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:persistence.properties")
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackageClasses = Application.class)
+class ApplicationConfig {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
 }
+
+
+
+
