98c45d4fecdb57435da319ff20c08b7adf10d5d3
Максимов Максим <mpmaximov@gmail.com>
2016-11-12 10:28:54 +0200

    add events

--- src/main/java/org/bionic/entity/Event.java
+++ src/main/java/org/bionic/entity/Event.java
@@ -1,5 +1,6 @@
 package org.bionic.entity;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -20,10 +22,19 @@ public class Event {
     @Id
     @GeneratedValue
     private long id;
+    @SerializedName("text")
     private String name;
+    @SerializedName("start_date")
     private Date start;
+    @SerializedName("end_date")
     private Date finish;
     private Date created;
     private String eventUrl;
 
+    @Override
+    public String toString() {
+        String startDate = (new SimpleDateFormat("dd.MM.yyyy")).format(start);
+        String finishDate = (new SimpleDateFormat("dd.MM.yyyy")).format(finish);
+        return (startDate.equals(finishDate)) ? "" + startDate + " " + name : "" + startDate + " - " + finishDate + " "  + name;
+    }
 }
