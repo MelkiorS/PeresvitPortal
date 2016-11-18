a83188f4123dcfcd70dc850cc786b01812557eb6
Volodymyr Lopukhovych <volodymyr.lopukhovychh@gmail.com>
2016-11-19 00:44:13 +0200

    adding my Way without makeup each html page 0.4

--- src/main/webapp/resources/frontend/workField/js/myWay.js
+++ src/main/webapp/resources/frontend/workField/js/myWay.js
@@ -1,6 +1,17 @@
 'use strict';
+function show_myWayBasic(){
+    // var private_office = $($('link[rel=import].private_office').prop('import')).find('.show').get(0);
+    $('.main_section').load('myWay/myWayBasic');
+    //при загрузке делаем кнопку 'Особистий кабінет' активной
+    for(var i in $('.my_way_sidebar_group_button')){
+        if($.trim($('.my_way_sidebar_group_button')[i].innerHTML) =='Базова техніка'){
+            $($('.my_way_sidebar_group_button')[i]).addClass('active_button');
+        }
+    }
+}
 $(function() {
 
+    show_myWayBasic
 
     // $('.my_way_sidebar_group_button').on('click', function () {
     //     $(this).addClass('active').removeClass('no_active');
