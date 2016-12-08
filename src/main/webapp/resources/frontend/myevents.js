fe0e409b80e9952105cce91b92817a473e069899
Максимов Максим <mpmaximov@gmail.com>
2016-12-08 20:22:57 +0200

    Merge branch 'mma-12'
    # Conflicts:
    #	src/main/webapp/WEB-INF/views/panel/myevents.html

--- src/main/webapp/resources/frontend/myevents.js
+++ src/main/webapp/resources/frontend/myevents.js
@@ -27,3 +27,14 @@ $(
         });
     }
 );
+
+function assignToMe(eventid) {
+    $.ajax({
+        type: "POST",
+        url: "/home/myWay/assignToMe",
+        data: {
+            "eventId": eventid
+        }
+    })
+}
+
