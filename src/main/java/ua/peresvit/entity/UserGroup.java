b32ab765a2583db48c8ac0f38720ae7a796074ed
Максимов Максим <mpmaximov@gmail.com>
2016-12-16 18:48:37 +0200

    User grousp

---
+++ src/main/java/ua/peresvit/entity/UserGroup.java
@@ -0,0 +1,37 @@
+package ua.peresvit.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by maximmaximov_2 on 15.12.16.
+ */
+@Entity
+@Table(name = "userGroup")
+@Data
+public class UserGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "user_groups", joinColumns = {
+            @JoinColumn(name = "usergroup_id", nullable = false, updatable = false)},
+            inverseJoinColumns = { @JoinColumn(name = "user_id", nullable = false, updatable = false) }
+    )
+    private Set<User> users;
+
+    @Override
+    public String toString() {
+        return "UserGroup{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", users=" + users +
+                '}';
+    }
+}
---
+++ src/main/java/ua/peresvit/service/impl/UserGroupServiceImpl.java
@@ -0,0 +1,43 @@
+package ua.peresvit.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ua.peresvit.entity.UserGroup;
+import ua.peresvit.dao.UserGroupRepository;
+import ua.peresvit.service.UserGroupService;
+
+import java.util.List;
+
+/**
+ * Created by maximmaximov_2 on 15.12.16.
+ */
+public class UserGroupServiceImpl implements UserGroupService {
+
+    @Autowired
+    private UserGroupRepository dao;
+
+    @Override
+    public List<UserGroup> findAll() {
+        return dao.findAll();
+    }
+
+    @Override
+    public UserGroup create(UserGroup e) {
+        return dao.save(e);
+    }
+
+    @Override
+    public UserGroup delete(UserGroup e) {
+        dao.delete(e);
+        return e;
+    }
+
+    @Override
+    public UserGroup update(UserGroup e) {
+        return dao.save(e);
+    }
+
+    @Override
+    public UserGroup findById(long id) {
+        return dao.findOne(id);
+    }
+}
